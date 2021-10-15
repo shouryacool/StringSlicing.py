f6e6986f9855fd6165f64ce6f28adcec86b5b549
shouryacool <84572733+shouryacool@users.noreply.github.com>
2021-10-15 09:55:44 +0530

    Add files via upload

---
+++ 02_ strings Slicing.py
@@ -0,0 +1,33 @@
+# greeting="Good Morning,"
+# name="Harry"
+# print(type(name))
+# # Concatining Two Strings
+# c=greeting+name
+# print(c)
+name="HarryIsGood"
+# Performing Slicing
+print(name[0:3])
+print(name [:5])  #is Same as [0:4]
+print(name [:4])  #is Same as [0:4]
+print(name [0:]) #is Same as [0:4]
+print(name [-5:-1]) # is same as name [0:4]
+print(name[-4:-2])  # is same as name [1:3]
+# Slicing with skip value
+d=name[1::2]
+print(d)
+
+# The index in a string starts from 0 to (length-1) in Python. To slice a string, we use the following syntax:
+# We can acess any Characters Of String but cant change it 
+
+# Negative Indices: Negative indices can also be used as shown in the figure above. -1 corresponds to the (length-1) index, -2 to (length-2).
+# Q Why To use Negative indices???
+# 
+# 
+# 
+# 
+
+
+
+
+
+
